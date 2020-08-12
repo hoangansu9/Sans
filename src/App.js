e87595dc327d529a10fd3dac273a68a5234c5f26
hoangansu9 <55569782+hoangansu9@users.noreply.github.com>
2020-08-12 11:34:10 +0700

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { Provider } from 'react-redux';
+import Game from './component/Game';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Home from './component/HomePage';
+const App = ({ store }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/game">Game</Link>
+          </li>
+        </ul>
 
+        <hr />
+        <div className="main-route-place">
+          <Route exact path="/" component={Home} />
+          <Route path="/game" component={Game}>
+            <Provider store={store}>
+              <Game />
+            </Provider>
+          </Route>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 export default App;
