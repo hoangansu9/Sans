84d1b64950d8aebb192d3914a96d07003ec779c0
hoangansu9 <55569782+hoangansu9@users.noreply.github.com>
2020-08-15 14:07:55 +0700

    fix_canvas

--- src/component/HomePage/index.js
+++ src/component/HomePage/index.js
@@ -1,49 +1,116 @@
-import React, { useState } from "react";
-import "./styles.scss";
-import Img1 from "../../images/paralax/1.png";
-import Img2 from "../../images/paralax/2.png";
-import Img3 from "../../images/paralax/3.png";
-import Img4 from "../../images/paralax/4.png";
-import Img5 from "../../images/paralax/5.png";
-import Img6 from "../../images/paralax/6.png";
-import Img7 from "../../images/paralax/7.png";
-import Img8 from "../../images/paralax/8.png";
-import Img9 from "../../images/paralax/9.png";
-import Img10 from "../../images/paralax/10.png";
-import Img11 from "../../images/paralax/11.png";
-import Img12 from "../../images/paralax/12.png";
-import GravitySticky from "./GravitySticky";
-
+import React, { useState } from 'react';
+import './styles.scss';
+import Img1 from '../../images/paralax/1.png';
+import Img2 from '../../images/paralax/2.png';
+import Img3 from '../../images/paralax/3.png';
+import Img4 from '../../images/paralax/4.png';
+import Img5 from '../../images/paralax/5.png';
+import Img6 from '../../images/paralax/6.png';
+import Img7 from '../../images/paralax/7.png';
+import Img8 from '../../images/paralax/8.png';
+import Img9 from '../../images/paralax/9.png';
+import Img10 from '../../images/paralax/10.png';
+import Img11 from '../../images/paralax/11.png';
+import Img12 from '../../images/paralax/12.png';
+import GravitySticky from './GravitySticky';
+import { useSpring, animated } from 'react-spring';
 const Home = () => {
-  const mouseOvesr = () => {
-    const parallax = (e) => {
-      this.querySelectorAll("layer").forEach((element) => {
-        const speed = element.getAttribute("data-speed");
-        const x = window.innerWidth - (e.pageX * speed) / 100;
-        const y = window.innerHeight - (e.pageY * speed) / 100;
-        element.style.transform = `translateX(${x}px) translateX(${y}px)`;
-      });
-    };
-    window.onmouseenter(parallax);
-  };
+  const calc = (x, y) => [
+    x - window.innerWidth / 2,
+    y - window.innerHeight / 2,
+  ];
+  const trans1 = (x, y) => `translate3d(${x / 10}px,${y / 10}px,0)`;
+  const trans2 = (x, y) => `translate3d(${x / 8 + 35}px,${y / 8 - 230}px,0)`;
+  const trans3 = (x, y) => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`;
+  const trans4 = (x, y) => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`;
+  const [props, set] = useSpring(() => ({
+    xy: [0, 0],
+    config: { mass: 10, tension: 550, friction: 140 },
+  }));
   return (
-    <div className="sans-comp-Home" onMouseEnter={() => mouseOvesr}>
-      <section className="paralax-section">
-        <img className="layer" data-speed="-5" alt="" src={Img1} />
-        <img className="layer" data-speed="5" alt="" src={Img2} />
-        <img className="layer" data-speed="2" alt="" src={Img3} />
-        <img className="layer" data-speed="6" alt="" src={Img4} />
-        <img className="layer" data-speed="8" alt="" src={Img5} />
-        <img className="layer" data-speed="-2" alt="" src={Img6} />
-        <img className="layer" data-speed="4" alt="" src={Img7} />
-        <img className="layer" data-speed="-9" alt="" src={Img8} />
-        <img className="layer" data-speed="6" alt="" src={Img9} />
-        <img className="layer" data-speed="-7" alt="" src={Img10} />
-        <img className="layer" data-speed="5" alt="" src={Img11} />
-        <img className="layer" data-speed="-5" alt="" src={Img12} />
-        <h2 className="layer" data-speed="2">
+    <div className="sans-comp-Home">
+      <section
+        className="paralax-section"
+        onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}
+      >
+        <animated.img
+          className="layer"
+          style={{ transform: props.xy.interpolate(trans1) }}
+          alt=""
+          src={Img1}
+        />
+        <animated.img
+          className="layer"
+          style={{ transform: props.xy.interpolate(trans2) }}
+          alt=""
+          src={Img2}
+        />
+        <animated.img
+          className="layer"
+          style={{ transform: props.xy.interpolate(trans3) }}
+          alt=""
+          src={Img3}
+        />
+        <animated.img
+          className="layer"
+          style={{ transform: props.xy.interpolate(trans4) }}
+          alt=""
+          src={Img4}
+        />
+        <animated.img
+          className="layer"
+          style={{ transform: props.xy.interpolate(trans2) }}
+          alt=""
+          src={Img5}
+        />
+        <animated.img
+          className="layer"
+          style={{ transform: props.xy.interpolate(trans4) }}
+          alt=""
+          src={Img6}
+        />
+        <animated.img
+          className="layer"
+          style={{ transform: props.xy.interpolate(trans1) }}
+          alt=""
+          src={Img7}
+        />
+        <animated.img
+          className="layer"
+          style={{ transform: props.xy.interpolate(trans3) }}
+          alt=""
+          src={Img8}
+        />
+        <animated.img
+          className="layer"
+          style={{ transform: props.xy.interpolate(trans4) }}
+          alt=""
+          src={Img9}
+        />
+        <animated.img
+          className="layer"
+          style={{ transform: props.xy.interpolate(trans2) }}
+          alt=""
+          src={Img10}
+        />
+        <animated.img
+          className="layer"
+          style={{ transform: props.xy.interpolate(trans2) }}
+          alt=""
+          src={Img11}
+        />
+        <animated.img
+          className="layer"
+          style={{ transform: props.xy.interpolate(trans1) }}
+          alt=""
+          src={Img12}
+        />
+        <animated.h2
+          className="layer"
+          style={{ transform: props.xy.interpolate(trans1) }}
+        >
           Welcome
-        </h2>
+        </animated.h2>
       </section>
       <GravitySticky />
     </div>
